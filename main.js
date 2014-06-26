82ec5971e08cdefe0a8231600c5e1a25f4afbd39
Swaff <swaff68@gmail.com>
2014-06-25 18:40:06 -0600

    finished bonus

--- main.js
+++ main.js
@@ -1,3 +1,85 @@
-$(document).on('ready', function() {
+// $(document).on('ready', function() {
   
+// });
+
+// alert("Please add information for each victim.");
+
+// var vicName = []
+// // var vicPhone = []
+// // var vicStreet = []
+
+
+// vicName.push(prompt("Victim Name (First and Last)"));
+// 	// vicPhone.push(prompt("Victim Phone Number"));
+// 	// vicStreet.push(prompt("Victim Street Address"));
+
+// var userResponse = confirm("Would you like to add another victim?");
+
+// while(userResponse === true){
+// 	vicName.push(prompt("Victim Name (First and Last)"));
+// 	// vicPhone.push(prompt("Victim Phone Number"));
+// 	// vicStreet.push(prompt("Victim Street Address"));
+// 	userResponse = confirm("Would you like to add another victim?");
+// }
+
+// var personInNeed = prompt("Who is in immediate need?");
+
+// alert("Sally, Joe, or Sam are on that street and could help " + personInNeed + ".");
+
+
+// $('header-area. button').toggle('light-box')
+
+$( "#target" ).click(function() {
+	$('#light-box1').css('display', 'table');
+		$('.victim-info input').val("");
+
 });
+
+
+$( "#close" ).click(function() {
+	$('#light-box1').hide();
+
+});
+
+$( "#save" ).click(function() {
+var userResponse = confirm("Would you like to save another student?");
+	if(userResponse === false){
+		$('#light-box1').hide();
+		$('.victim-info input').val("");
+		$('#light-box2').css('display', 'table');
+		alert("Give us your volunteer info.")
+	}
+
+	else{
+		$('.victim-info input').val("");
+	}
+	return false;
+
+
+
+});
+
+$( "#close2" ).click(function() {
+	$('#light-box2').hide();
+	$('.volunteer-info input').val("");
+
+});
+
+$("#save2").click(function(){
+var userResponse = confirm("Thanks, now go save those students!");
+	if(userResponse === false || true){
+		$('#light-box2').hide();
+		$('.volunteer-info input').val("");
+
+	}
+
+	else {
+		$('.volunteer-info input').val("");
+	}
+	return false;
+
+
+});
+
+
+
